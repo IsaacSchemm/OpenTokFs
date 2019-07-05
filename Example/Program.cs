4b89042c1d7ffa08c3a657c794f4cfec751ef123
Isaac Schemm <isaacschemm@gmail.com>
2019-07-05 15:55:39 -0500

    Add sandbox

---
+++ Example/Program.cs
@@ -0,0 +1,31 @@
+﻿using ISchemm.OpenTokFs;
+using System;
+using System.Threading.Tasks;
+
+using Requests = ISchemm.OpenTokFs.Requests;
+
+namespace Example
+{
+    class OpenTokCredentials : IOpenTokCredentials
+    {
+        public int ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var credentials = new OpenTokCredentials();
+
+            Console.Write("Enter your API key: ");
+            credentials.ApiKey = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your API secret: ");
+            credentials.ApiSecret = Console.ReadLine();
+
+            var broadcasts = await Requests.Broadcast.ListAsync(credentials);
+            Console.WriteLine(broadcasts);
+        }
+    }
+}
