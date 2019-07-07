564ae02e55fe9ad325f7bf459bc84de04675c549
Isaac Schemm <isaacschemm@gmail.com>
2019-07-07 13:19:40 -0500

    Define request types in C#

---
+++ ISchemm.OpenTokFs.RequestTypes/BroadcastStartRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ISchemm.OpenTokFs.RequestTypes
+{
+    public interface IBroadcastStartRequest
+    {
+        string SessionId { get; }
+        string LayoutType { get; }
+        string LayoutStylesheet { get; }
+        TimeSpan Duration { get; }
+        bool Hls { get; }
+        IEnumerable<IRtmpDestination> Rtmp { get; }
+        string Resolution { get; }
+    }
+
+    public class BroadcastStartRequest : IBroadcastStartRequest
+    {
+        public string SessionId { get; }
+        public string LayoutType { get; set; }
+        public string LayoutStylesheet { get; set; }
+        public TimeSpan Duration { get; set; }
+        public bool Hls { get; set; }
+        public IEnumerable<IRtmpDestination> Rtmp { get; set; }
+        public string Resolution { get; set; }
+
+        public BroadcastStartRequest(string sessionId)
+        {
+            this.SessionId = sessionId ?? throw new ArgumentNullException(nameof(sessionId));
+            this.LayoutType = "bestFit";
+            this.LayoutStylesheet = null;
+            this.Duration = TimeSpan.FromHours(2);
+            this.Hls = false;
+            this.Rtmp = Enumerable.Empty<IRtmpDestination>();
+            this.Resolution = "640x480";
+        }
+    }
+}
---
+++ ISchemm.OpenTokFs.RequestTypes/RtmpDestination.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ISchemm.OpenTokFs.RequestTypes
+{
+    public interface IRtmpDestination
+    {
+        string Id { get; }
+        string ServerUrl { get; }
+        string StreamName { get; }
+    }
+
+    public class RtmpDestination : IRtmpDestination
+    {
+        public string Id { get; set; }
+        public string ServerUrl { get; }
+        public string StreamName { get; }
+
+        public RtmpDestination(string serverUrl, string streamName)
+        {
+            this.Id = null;
+            this.ServerUrl = serverUrl ?? throw new ArgumentNullException(nameof(serverUrl));
+            this.StreamName = streamName ?? throw new ArgumentNullException(nameof(streamName));
+        }
+    }
+}
