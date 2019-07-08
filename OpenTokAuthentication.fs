4a974bcfb3c17401d0583adb96de676f5a62aa41
Isaac Schemm <isaacschemm@gmail.com>
2019-07-08 13:54:01 -0500

    Remove JWT library dependency

--- OpenTokAuthentication.fs
+++ OpenTokAuthentication.fs
@@ -1,10 +1,43 @@
 namespace OpenTokFs
 
 open System
-open JWT
-open JWT.Algorithms
-open JWT.Serializers
 open System.Net
+open System.Security.Cryptography
+
+// http://www.fssnip.net/7RK/title/Creating-and-validating-JWTs-in-just-35-lines-of-F-code
+module internal JsonWebToken =
+    open System.Text
+    open System.Text.RegularExpressions
+    let replace (oldVal: string) (newVal: string) = fun (s: string) -> s.Replace(oldVal, newVal)
+    let minify = 
+        let regex = Regex("(\"(?:[^\"\\\\]|\\\\.)*\")|\\s+", RegexOptions.Compiled|||RegexOptions.CultureInvariant)
+        fun s ->
+            regex.Replace(s, "$1")
+    let base64UrlEncode bytes =
+        Convert.ToBase64String(bytes) |> replace "+" "-" |> replace "/" "_" |> replace "=" ""
+    type IJwtAuthority =
+        inherit IDisposable
+        abstract member IssueToken: header:string -> payload:string -> string
+        abstract member VerifyToken: string -> bool
+    let newJwtAuthority (initAlg: byte array -> HMAC) key =
+        let alg = initAlg(key)
+        let encode = minify >> Encoding.UTF8.GetBytes >> base64UrlEncode
+        let issue header payload =
+            let parts = [header; payload] |> List.map encode |> String.concat "."
+            let signature = parts |> Encoding.UTF8.GetBytes |> alg.ComputeHash |> base64UrlEncode
+            [parts; signature] |> String.concat "."
+        let verify (token: string) =
+            let secondDot = token.LastIndexOf(".")
+            let parts = token.Substring(0, secondDot)
+            let signature = token.Substring(secondDot + 1)
+            (parts |> Encoding.UTF8.GetBytes |> alg.ComputeHash |> base64UrlEncode) = signature
+
+        {
+            new IJwtAuthority with
+                member __.IssueToken header payload = issue header payload
+                member __.VerifyToken token = verify token
+                member __.Dispose() = alg.Dispose()
+        }
 
 /// <summary>
 /// Internal functions for authenticating with OpenTok.
@@ -26,11 +59,14 @@ module OpenTokAuthentication =
             exp = DateTimeOffset.UtcNow.AddMinutes(3.0).ToUnixTimeSeconds()
             jti = Guid.NewGuid().ToString()
         }
-        let algorithm = new HMACSHA256Algorithm()
-        let serialzier = new JsonNetSerializer()
-        let urlEncoder = new JwtBase64UrlEncoder()
-        let encoder = new JwtEncoder(algorithm, serialzier, urlEncoder)
-        encoder.Encode(payload, credentials.ApiSecret)
+        let encodedSecret = credentials.ApiSecret |> System.Text.Encoding.UTF8.GetBytes
+        let testAuth = JsonWebToken.newJwtAuthority (fun key -> new HMACSHA256(key) :> HMAC) encodedSecret
+        let header = 
+            """{
+                "alg": "HS256",
+                "typ": "JWT"
+            }"""
+        testAuth.IssueToken header (Newtonsoft.Json.JsonConvert.SerializeObject payload)
 
     let BuildRequest (credentials: IOpenTokCredentials) (path: string) (query: seq<string>) =
         let req =
