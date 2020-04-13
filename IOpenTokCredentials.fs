7fec0abbbe7bc03f072b5dceb5908dcc0e375308
Isaac Schemm <isaacschemm@gmail.com>
2020-04-13 08:18:51 -0500

    Update documentation

--- IOpenTokCredentials.fs
+++ IOpenTokCredentials.fs
@@ -1,15 +1,11 @@
 ﻿namespace OpenTokFs
 
-/// <summary>
 /// Any object that provides an API key and secret for the OpenTok REST API.
-/// </summary>
 type IOpenTokCredentials =
     abstract member ApiKey: int
     abstract member ApiSecret: string
 
-/// <summary>
 /// A record that provides an API key and secret for the OpenTok REST API.
-/// </summary>
 type OpenTokCredentials = {
     apiKey: int
     apiSecret: string
--- OpenTokPagingParameters.fs
+++ OpenTokPagingParameters.fs
@@ -2,16 +2,10 @@
 
 open System
 
-/// <summary>
 /// A set of parameters to control which section of a list is returned from a request.
-/// </summary>
 type OpenTokPagingParameters = {
-    /// <summary>
     /// The start offset in the list.
-    /// </summary>
     offset: int
-    /// <summary>
     /// The number of items to retrieve, starting at the offset.
-    /// </summary>
-    count: Nullable<int>
+    count: int Nullable
 }
