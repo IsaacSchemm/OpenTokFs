2cfc35ba8dd8961d67693fa3ad999a05bde8392e
Isaac Schemm <isaacschemm@gmail.com>
2020-09-27 16:29:35 -0500

    Add OpenTokFs.Credentials to actions

--- OpenTokFs.Credentials/IAccountCredentials.cs
+++ OpenTokFs.Credentials/IAccountCredentials.cs
@@ -1,6 +1,16 @@
 ﻿namespace OpenTokFs.Credentials {
+    /// <summary>
+    /// A set of account-level credentials for Vonage Video API.
+    /// An implementation of this interface exists in the OpenTokFs library (OpenTokAccountCredentials).
+    /// </summary>
     public interface IAccountCredentials {
+        /// <summary>
+        /// The account-level API key.
+        /// </summary>
         int ApiKey { get; }
+        /// <summary>
+        /// The account-level API secret.
+        /// </summary>
         string ApiSecret { get; }
     }
 }
--- OpenTokFs.Credentials/IProjectCredentials.cs
+++ OpenTokFs.Credentials/IProjectCredentials.cs
@@ -1,6 +1,16 @@
 ﻿namespace OpenTokFs.Credentials {
+    /// <summary>
+    /// A set of project-level credentials for Vonage Video API.
+    /// An implementation of this interface exists in the OpenTokFs library (OpenTokProjectCredentials).
+    /// </summary>
     public interface IProjectCredentials {
+        /// <summary>
+        /// The project ID / API key.
+        /// </summary>
         int ApiKey { get; }
+        /// <summary>
+        /// The API secret.
+        /// </summary>
         string ApiSecret { get; }
     }
 }
